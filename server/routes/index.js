c404be5ecd61b4c1bcc813339d7434ae6d1d28b9
namdevjagtap <namdevjagtap9604@gmail.com>
2020-02-07 12:27:22 +0530

    initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+/*router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Express'
   });
-});
+});*/
+
+router.all('/*', function(req, res, next) {
+  console.log('Intercepting requests ...');
+  next();  // call next() here to move on to next middleware/router
+})
 
 module.exports = router;
